24382f89a8394b1856b37dcc68402688f2ce60c3
jiangyupeng <jiangyupeng@bjsasc.com>
2020-02-08 20:36:01 +0800

    driver

--- src/com/chiang/inter/entity/Driver.java
+++ src/com/chiang/inter/entity/Driver.java
@@ -4,10 +4,16 @@ import com.chiang.inter.ICar;
 import com.chiang.inter.IDriver;
 
 public class Driver implements IDriver {
-
+	static final int MAXIMUM_CAPACITY = 1 << 30;
 	@Override
 	public void drive(ICar car) {
 		car.run();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(MAXIMUM_CAPACITY);
+		Driver driver = new Driver();
+		System.out.print(driver.MAXIMUM_CAPACITY);
 	}
 
 }
