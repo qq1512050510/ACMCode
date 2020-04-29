23bcb3e40d735cc65faa0cb46a9be686dd3c1e0a
jiangyupeng <765819328@qq.com>
2020-04-29 21:49:58 +0800

    Prototype Pattern

---
+++ src/com/chiang/prototype/AdvTemplate.java
@@ -0,0 +1,14 @@
+package com.chiang.prototype;
+
+public class AdvTemplate {
+    private String advSubject = "**银行活动！";
+    private String advContent = "五一活动，冲100给100";
+
+    public String getAdvSubject() {
+        return advSubject;
+    }
+
+    public String getAdvContent() {
+        return advContent;
+    }
+}
---
+++ src/com/chiang/prototype/Client.java
@@ -0,0 +1,32 @@
+package com.chiang.prototype;
+
+import java.lang.String;
+import java.util.Random;
+
+public class Client {
+    private final static int MAX_MAIL = 100;
+    private static void send(Mail mail){
+        System.out.println("发送邮件:"+mail.getReceiver());
+    }
+    private static String getRandString(int count){
+        String source = "abcdefghijklmnopqrstuvwxyz";
+        StringBuffer stringBuffer = new StringBuffer();
+        for(int i=0;i<count;i++){
+            stringBuffer.append(source.charAt(new Random().nextInt(source.length())));
+        }
+        return stringBuffer.toString();
+    }
+    public static void main(String[] args) {
+        AdvTemplate advTemplate = new AdvTemplate();
+        Mail mail = new Mail(advTemplate);
+        int i = 0;
+        while(i<100){
+            Mail mailClone = mail.clone();
+            System.out.print(mailClone.hashCode());
+            System.out.print(mail.hashCode());
+            mail.setReceiver(getRandString(10));
+            send(mail);
+            i++;
+        }
+    }
+}
