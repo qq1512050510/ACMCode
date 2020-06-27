1d144ddf1961ba599d1cceb0d04ce0e7adf9fb44
jiangyupeng <765819328@qq.com>
2020-06-27 16:40:54 +0800

    Thread

--- src/com/chiang/thread/ThreadDaemon.java
+++ src/com/chiang/thread/ThreadDaemon.java
@@ -1,7 +1,7 @@
 package com.chiang.thread;
 
 public class ThreadDaemon {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread() {
             @Override
             public void run() {
@@ -10,6 +10,10 @@ public class ThreadDaemon {
                 }
             }
         };
+
+        ThreadDaemon threadTest = new ThreadDaemon();
+        threadTest.wait();
+        threadTest.notify();
 
         thread.setDaemon(true);
         thread.start();
