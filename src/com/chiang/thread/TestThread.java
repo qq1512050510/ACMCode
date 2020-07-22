1ca408bdae0f23abc0543144fbf703bb5b8e4c8d
jiangyupeng <765819328@qq.com>
2020-07-22 09:50:26 +0800

    Future,log,Thread Run

--- src/com/chiang/thread/TestThread.java
+++ src/com/chiang/thread/TestThread.java
@@ -4,10 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.Collection;
 import java.util.Stack;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 //@Slf4j(topic = "c.Test")
 public class TestThread {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         Runnable r = new Runnable() {
             @Override
             public void run() {
@@ -27,9 +30,36 @@ public class TestThread {
         sb.append("cde");
         System.out.println(sb.toString());
         Stack stack = new Stack();
-        stack.peek();
+        //stack.peek();
+        //FutureTask
 
+        FutureTask<Integer> task = new FutureTask<>(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                System.out.printf("%s...","Running");
+                Thread.sleep(1000);
+                return 100;
+            }
+        });
+        Thread tf = new Thread(task,"tf1");
 
+        tf.start();
+        System.out.printf("返回结果：%d",task.get());
+        Runnable joinTest = ()->{
+            System.out.println("JoinTest");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("JoinTest End");
+        };
+        Thread tr = new Thread(joinTest,"tr1");
+        //joinTest.run();
+        tr.start();
+        tr.join();
+        Thread.sleep(500);
+        System.out.println("主线程结束");
 
     }
 }
