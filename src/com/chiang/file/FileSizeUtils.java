0be5e5413979674efa36c9bd949376c909a0cc3b
jiangyupeng <765819328@qq.com>
2021-03-04 09:13:59 +0800

    base64编码

--- src/com/chiang/file/FileSizeUtils.java
+++ src/com/chiang/file/FileSizeUtils.java
@@ -1,6 +1,10 @@
 package com.chiang.file;
 
-import java.io.File;
+import org.apache.commons.codec.binary.Base64;
+import sun.misc.BASE64Encoder;
+
+import javax.imageio.stream.FileImageInputStream;
+import java.io.*;
 import java.util.Locale;
 
 public class FileSizeUtils {
@@ -31,12 +35,44 @@ public class FileSizeUtils {
 		return String.format(Locale.ENGLISH, "%.1f", pre);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		//String path = "./src/gag_evt_202001-12_v00.fits";
-		String path = "./src/testFile";
+		//String path = "./src/testFile";
+		String path = "./src/photo.jpg";
+
 		File file = new File(path);
 		System.out.println(file.length());
 		System.out.println(FileSizeUtils.byteFormat(2048, true));
+		FileImageInputStream fileImageInputStream = new FileImageInputStream(file);
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		byte[]buf = new byte[1024];
+		int num = 0;
+		while((num = fileImageInputStream.read(buf))!=-1){
+			outputStream.write(buf,0,num);
+		}
+		byte[] out = outputStream.toByteArray();
+		byte[] aa = Base64.decodeBase64(out);
+		System.out.println();
+		BASE64Encoder encoder = new BASE64Encoder();
+		System.out.println(encoder.encode(out).length());
+		outputStream.close();
+		fileImageInputStream.close();
+
+		InputStream in = new FileInputStream(path);
+		System.out.println("文件大小（字节）="+in.available());
+		byte[] data = new byte[in.available()];
+		in.read(data);
+		in.close();
+
+		// 对字节数组进行Base64编码，得到Base64编码的字符串
+		//BASE64Encoder encoder = new BASE64Encoder();
+		String base64Str = encoder.encode(data);
+		System.out.println(base64Str);
+		File fileOut = new File("./src/out.txt");
+		FileOutputStream fileOutputStream = new FileOutputStream(fileOut);
+		fileOutputStream.write(base64Str.getBytes());
+		fileOutputStream.close();
+
 	}
 
 }
